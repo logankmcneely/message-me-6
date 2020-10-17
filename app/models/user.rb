b94cb5c86cea7e19a7e3a0325bf5e9b55135c129
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-17 16:02:17 -0500

    added user messages with test entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :username,
     presence: true,
-    uniqueness: true,
+    uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 25 }
   has_secure_password
+  has_many :messages
 end
