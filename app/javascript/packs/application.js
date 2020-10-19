ca939cb2683514b47a85ff0d842702a68a2e79d7
Logan McNeely <logan.k.mcneely@gmail.com>
2020-10-19 10:07:24 -0500

    added auth system with message feedback

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,7 +20,12 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
+
+
 
 
 
